1e0e536a7225b79ed1834aee5613de3ef7987bb6
Milly Farouki <milly.farouki@featurespace.co.uk>
2021-12-05 19:25:53 +0000

    Initialise hello_mnist with simple neural net

---
+++ run.sh
@@ -0,0 +1,5 @@
+#source ./utilities/initialise.sh
+
+python python_scripts/hello_mnist.py
+
+#source ./utilities/initialise.sh
---
+++ utilities/constants.sh
@@ -0,0 +1,2 @@
+CONDA_PATH="/home/ds_millyfarouki/miniconda3/etc/profile.d/conda.sh"
+tf_venv="tensorflow"
