29eb2e9ad4c7f475ee7ed5323b4c7f77c9b36ba9
Milly <millyfarouki@gmail.com>
2021-12-15 19:21:33 -0800

    Load in raw datasets and explore samples

---
+++ sentiment_analysis/load_raw_datasets.py
@@ -0,0 +1,34 @@
+import tensorflow as tf
+from utilities import display_info
+
+seed = 42
+
+
+def define_label(raw_train_ds: tf.data.Dataset, label: int):
+    if (label >= len(raw_train_ds.class_names)) or (label < 0):
+        raise ValueError(f'Label {label} not recognised in dataset class names')
+    return raw_train_ds.class_names[label]
+
+
+def build_raw_datasets(directory: str, batch_size=32, validation_split=0.2):
+    display_info(f'Reading train, validation, and test sets from {directory}/train')
+    train_dataset = tf.keras.preprocessing.text_dataset_from_directory(
+        f'{directory}/train', batch_size=batch_size, validation_split=validation_split, subset='training', seed=seed)
+    validation_dataset = tf.keras.preprocessing.text_dataset_from_directory(
+        f'{directory}/train', batch_size=batch_size, validation_split=validation_split, subset='validation', seed=seed)
+    test_dataset = tf.keras.preprocessing.text_dataset_from_directory(f'{directory}/test', batch_size=batch_size)
+    return train_dataset, validation_dataset, test_dataset
+
+
+def view_dataset(raw_ds: tf.data.Dataset, n_reviews=3):
+    n_batches = tf.data.experimental.cardinality(raw_ds)
+    display_info(f'Viewing the first {n_reviews} reviews in the first batch (of {n_batches} total batches)')
+    for text_batch, label_batch in raw_ds.take(1):
+        batch_size = text_batch.shape[0]
+        if n_reviews > batch_size:
+            raise ValueError(f'Cannot show {n_reviews} reviews, because a batch only contains {batch_size} reviews')
+        for i in range(n_reviews):
+            review, label = text_batch.numpy()[i], label_batch.numpy()[i]
+            print(f'Review: {review}')
+            print(f'Label: {label}, indicating a {define_label(raw_ds, label)} review')
+
--- sentiment_analysis/sentiment_analysis.py
+++ sentiment_analysis/sentiment_analysis.py
@@ -1,6 +1,7 @@
 import os
 
 from download_and_view import *
+from load_raw_datasets import *
 
 imdb_url = "https://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz"
 expected_top_level_contents = ['train', 'test', 'README', 'imdbEr.txt', 'imdb.vocab']
@@ -8,12 +9,19 @@ expected_train_contents = ['labeledBow.feat', 'urls_pos.txt', 'urls_unsup.txt', 
                            'urls_neg.txt', 'neg']
 
 
-def main():
+def create_directory_structure():
     dataset_dir = download_and_unzip("aclImdb_v1", imdb_url, 'aclImdb')
     check_directory_contents(dataset_dir, expected_top_level_contents)
     train_dir = os.path.join(dataset_dir, 'train')
     read_random_review(train_dir)
     remove_unneeded_directories(train_dir, 'unsup')
+    return dataset_dir
+
+
+def main():
+    dataset_dir = create_directory_structure()
+    raw_train_dataset, raw_validation_dataset, raw_test_dataset = build_raw_datasets(dataset_dir)
+    view_dataset(raw_validation_dataset)
 
 
 if __name__ == "__main__":
